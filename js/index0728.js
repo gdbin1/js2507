57d7c45416b3d922fe3127e80e1bea70c472b680
gdbin1 <ot5037@naver.com>
2025-07-28 20:27:54 +0900

    0728

--- js/index0728.js
+++ js/index0728.js
@@ -4,9 +4,13 @@ let pool = 35000;
 let park = 20000;
 let result;
 // ------------------------------
-result = hotel + pool * 0.95;
-d.getElementById('p1').innerHTML = `<b>첫번째 패키지: 숙박+수영장 5%할인 ${result}</b>`;
-result = hotel + pool + park * 0.8;
-d.getElementById('p3').innerHTML = `<b>첫번째 패키지:${result}</b>`;
+// 패키지1
+result = hotel + pool * 0.85;
+d.getElementById('p1').innerHTML = `<b>첫번째 패키지: 숙박+수영장 15%할인 ${result}</b>`;
+// 패키지2
+result = pool + park *0.9
+d.getElementById('p2').innerHTML = `<b>두번째: 수영장+파크 패키지 종일 이용권 5%할인 ${result}</b>`;
+// 패키지3
+result = hotel + pool + park * 0.85; 
+d.getElementById('p3').innerHTML = `<b>세번째: 숙박+수영장+파크 풀패키지 20%할인${result}</b>`;
 
-result = hotel + park *0.9
